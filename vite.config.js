2b1a6540a804595ba08df31f777d4bd35120d6fc
Tom <tom@tom.com>
2025-11-26 23:00:14 +0000

    Convert to Vite

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: './',          // Project root - where to find index.html
+  build: {
+    outDir: 'dist',    // Output path
+  }
+});
