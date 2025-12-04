93fa3a7e91e3daa30a45d62175a8098b510db849
Tom <tom@tom.com>
2025-12-04 00:23:12 +0000

    Convert mothership to class

--- src/modules/Mothership/Mothership.js
+++ src/modules/Mothership/Mothership.js
@@ -1,43 +1,60 @@
 import Ctx from '../Ctx/Ctx';
 import Sprite from '../Sprite/Sprite';
 import Sounds from '../Sounds/Sounds';
+import { MOTHERSHIP } from '../../config';
 
+export default class Mothership {
+    constructor() {
+        this.config = MOTHERSHIP;
+        this.ctx = Ctx();
+        this.sprite = Sprite();
+        this.currentAnimationFrame = 0; // Current animation frame from animationframes array
+        this.animationFrameNo = 0; // Current frame count
+        this.isActive = false;
+        this.score = Math.ceil(Math.random() * 10) * 100;
+        this.width = this.config.width;
+        this.height = this.config.height;
+        this.speed = this.config.speed;
+        this.x = -this.width;
+        this.y = this.config.y;
+        this.animationFrames = this.config.animationFrames;
+        this.spriteX = this.config.spriteX;
+        this.spriteY = this.config.spriteY;
+        this.isExploding = false;
+        this.noExplodingFrames = this.config.noExplodingFrames;
+        this.spriteExplosionX = this.config.spriteExplosionX;
+        this.spriteExplosionY = this.config.spriteExplosionY;
+        this.spriteExplosionWidth = this.config.spriteExplosionWidth;
+        this.spriteExplosionHeight = this.config.spriteExplosionHeight;
+    }
 
-const mothership = () => {
-    const destroySound = Sounds();
-    destroySound.startTime = 7.05;
-    destroySound.stopTime = 7.95;
+    move() {
+        this.x += this.speed;
+    }
 
-    return {
-        ctx: Ctx(),
-        sprite: Sprite(),
-        isExploding: false,
-        currentAnimationFrame: 0, // Current animation frame from animationframes array
-        animationFrameNo: 0, // Current frame count
-        isActive: false,
-        score: Math.ceil(Math.random() * 10) * 100,
-        move: function () {
-            this.x += this.speed;
-        },
-        reset: function () {
-            this.x = -this.width;
-            this.isActive = true;
-            this.score = Math.ceil(Math.random() * 10) * 100;
-        },
-        remove: function () {
-            this.isActive = false;
-        },
-        destroy: function () {
-            destroySound.play();
-            // Set isAnimating frame to start animation
-            this.isExploding = 0;
-        },
-        purge: function () {
-            if (!this.isActive) {
-                this.remove();
-            }
-        },
-        render: function () {
+    reset() {
+        this.x = -this.width;
+        this.isActive = true;
+        this.score = Math.ceil(Math.random() * 10) * 100;
+    }
+
+    remove() {
+        this.isActive = false;
+    }
+
+    destroy() {
+        // destroySound.play();
+        // Set isAnimating frame to start animation
+        this.isExploding = 0;
+    }
+
+    purge() {
+        if (!this.isActive) {
+            this.remove();
+        }
+    }
+
+    render() {
             // Centralise explosion sprite
             const xSpriteOffset = (this.width - this.spriteExplosionWidth) / 2;
             const ySpriteOffset = (this.height - this.spriteExplosionHeight) / 2;
@@ -62,7 +79,7 @@ const mothership = () => {
 
                     this.ctx.drawImage(this.sprite, this.spriteX, this.spriteY + (this.height * this.currentAnimationFrame), this.width, this.height, this.x, this.y, this.width, this.height);
                 } else {
-                    // Exploision
+                    // Explosion
                     this.ctx.drawImage(this.sprite, this.spriteExplosionX, this.spriteExplosionY, this.spriteExplosionWidth, this.spriteExplosionHeight, this.x + xSpriteOffset, this.y + ySpriteOffset, this.spriteExplosionWidth, this.spriteExplosionHeight);
                     // Score
                     
@@ -75,7 +92,4 @@ const mothership = () => {
                 }
             }
         }
-    };
-};
-
-export default mothership;
+}
