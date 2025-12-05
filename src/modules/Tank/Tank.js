50a98d0e5e338b8568b965346cc62c32f99bbd35
Tom <tom@tom.com>
2025-12-05 22:08:54 +0000

    Refactor tank to class

--- src/modules/Tank/Tank.js
+++ src/modules/Tank/Tank.js
@@ -1,79 +1,121 @@
-import { SCREEN, TANK  } from '../../config';
+import { SCREEN, TANK } from '../../config';
 import Ctx from '../Ctx/Ctx';
 import Sprite from '../Sprite/Sprite';
 import Sounds from '../Sounds/Sounds';
 
-const tank = () => {
+export default class Tank {
+    constructor() {
+        this.config = TANK;
+        this.screenConfig = SCREEN;
+        this.ctx = Ctx();
+        this.sprite = Sprite();
+        this.animationFrame = 0;
+        this.animationType = null;
+        this.destroyAnimationFrames = 100; // TODO - move this to config
+        this.shootAnimationFrames = 10; // TODO - move this to config
+        this.explodeSound = Sounds();
+        this.explodeSound.startTime = 7.05; // TOPDO - move this to config
+        this.explodeSound.stopTime = 7.95; // TODO - move this to config
+        this.speed = this.config.speed;
+        this.width = this.config.width;
+        this.height = this.config.height;
+        this.x = this.config.x;
+        this.y = this.config.y;
+        this.spriteInfo = this.config.spriteInfo;
+        this.xSpriteOffset = (this.width - this.spriteInfo.explosionWidth) / 2;
+        this.ySpriteOffset = (this.height - this.spriteInfo.explosionHeight) / 2;
+    }
 
-    const explodeSound = Sounds();
-    explodeSound.startTime = 7.05;
-    explodeSound.stopTime = 7.95;
-
-    return {
-        ctx: Ctx(),
-        sprite: Sprite(),
-        animationFrame: 0,
-        animationType: null,
-        destroyAnimationFrames: 100,
-        shootAnimationFrames: 10,
-        move: function (currentKeysPressed) {
-            const isLeftKey = currentKeysPressed.findIndex((key) => {
-                return key === 37;
-            });
-            const isRightKey = currentKeysPressed.findIndex((key) => {
-                return key === 39;
-            });
-            if (isLeftKey > -1) {
-                if (this.x > 0) {
-                    this.x -= this.speed;
-                }
-            }
-            if (isRightKey > -1) {
-                if (this.x < SCREEN.width - this.width) {
-                    this.x += this.speed;
-                }
-            }
-        },
-        destroy: function () {
-            // Set isAnimating frame to start animation
-            explodeSound.play();
-            this.animationFrame = 1;
-            this.animationType = 'destroy';
-        },
-        reset: function () {
-            this.x = TANK.x;
-            this.animationFrame = 0;
-            this.animationType = null;
-        },
-        render: function () {
-            // Centralise explosion sprite
-            const xSpriteOffset = (this.width - this.spriteInfo.explosionWidth) / 2;
-            const ySpriteOffset = (this.height - this.spriteInfo.explosionHeight) / 2;
-
-            if (this.animationFrame === 0) {
-                this.ctx.drawImage(this.sprite, this.spriteInfo.x, this.spriteInfo.y, this.width, this.height, this.x, this.y, this.width, this.height);
-            } else if (this.animationType) {
-                switch (this.animationType) {
-                    case 'destroy':
-                        this.ctx.drawImage(this.sprite, this.spriteInfo.explosionX, this.spriteInfo.explosionY, this.spriteInfo.explosionWidth, this.spriteInfo.explosionHeight, this.x + xSpriteOffset, this.y + ySpriteOffset, this.spriteInfo.explosionWidth, this.spriteInfo.explosionHeight);
-                        this.animationFrame++;
-                        if (this.animationFrame > this.destroyAnimationFrames) {
-                            this.animationFrame = 0;
-                            this.animationType = null;
-                        }
-                        break;
-                    case 'shoot':
-                        this.ctx.drawImage(this.sprite, 0, this.height, this.width, this.height, this.x, this.y, this.width, this.height);
-                        this.animationFrame++;
-                        if (this.animationFrame > this.shootAnimationFrames) {
-                            this.animationFrame = 0;
-                            this.animationType = null;
-                        }
-                        break;
-                }
+    move(currentKeysPressed) {
+        const isLeftKey = currentKeysPressed.findIndex((key) => {
+            return key === 37;
+        });
+        const isRightKey = currentKeysPressed.findIndex((key) => {
+            return key === 39;
+        });
+        if (isLeftKey > -1) {
+            if (this.x > 0) {
+                this.x -= this.speed;
             }
         }
-    };
-};
+        if (isRightKey > -1) {
+            if (this.x < this.screenConfig.width - this.width) {
+                this.x += this.speed;
+            }
+        }
+    }
 
-export default tank;
+    destroy() {
+        // Set isAnimating frame to start animation
+        // this.explodeSound.play(); 
+        this.animationFrame = 1;
+        this.animationType = 'destroy';
+    }
+
+    reset() {
+        this.x = this.config.x;
+        this.animationFrame = 0;
+        this.animationType = null;
+    }
+
+    render() {
+        if (this.animationFrame === 0) {
+            this.ctx.drawImage(
+                this.sprite,
+                this.spriteInfo.x,
+                this.spriteInfo.y,
+                this.width,
+                this.height,
+                this.x,
+                this.y,
+                this.width,
+                this.height
+
+            );
+        } else if (this.animationType) {
+            switch (this.animationType) {
+
+                case 'destroy':
+                    this.ctx.drawImage(
+                        this.sprite,
+                        this.spriteInfo.explosionX,
+                        this.spriteInfo.explosionY,
+                        this.spriteInfo.explosionWidth,
+                        this.spriteInfo.explosionHeight,
+                        this.x + this.xSpriteOffset,
+                        this.y + this.ySpriteOffset,
+                        this.spriteInfo.explosionWidth,
+                        this.spriteInfo.explosionHeight
+
+                    );
+                    this.animationFrame++;
+                    if (this.animationFrame > this.destroyAnimationFrames) {
+                        this.animationFrame = 0;
+                        this.animationType = null;
+                    }
+                    break;
+                case 'shoot':
+                    this.ctx.drawImage(
+                        this.sprite,
+                        0,
+                        this.height,
+                        this.width,
+                        this.height,
+                        this.x,
+                        this.y,
+                        this.width,
+                        this.height
+
+                    );
+                    this.animationFrame++;
+                    if (this.animationFrame > this.shootAnimationFrames) {
+                        this.animationFrame = 0;
+                        this.animationType = null;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
